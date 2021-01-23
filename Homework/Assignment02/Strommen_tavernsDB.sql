d4dc08bcaee0f2572d01296909f458b0f462be57
Aja Strommen <a.g.mechanix@gmail.com>
2021-01-22 20:55:24 -0500

    fixed FK conflicts & Identity errors

--- Homework/Assignment02/Strommen_tavernsDB.sql
+++ Homework/Assignment02/Strommen_tavernsDB.sql
@@ -11,11 +11,17 @@ CREATE TABLE unit (
   name VARCHAR(128) NOT NULL,
 );
 
+ALTER TABLE unit ADD PRIMARY KEY (id);
+
 CREATE TABLE supplies (
   id int NOT NULL IDENTITY(1,1),
   name VARCHAR(128) NOT NULL,
   unit_id int NOT NULL,
 );
+
+ALTER TABLE supplies ADD PRIMARY KEY (id);
+ALTER TABLE supplies ADD FOREIGN KEY (unit_id) REFERENCES unit(id);
+
 
 CREATE TABLE receivables (
   id int NOT NULL IDENTITY(1,1),
@@ -24,8 +30,14 @@ CREATE TABLE receivables (
   qty int NOT NULL,
   cost float NOT NULL,
   created DATETIME NOT NULL
-                DEFAULT CURRENT_TIMESTAMP, 
+    DEFAULT CURRENT_TIMESTAMP, 
 );
+
+ALTER TABLE receivables ADD PRIMARY KEY (id);
+--ALTER TABLE receivables ADD FOREIGN KEY (tavern_id) REFERENCES tavern(id);
+ALTER TABLE receivables ADD FOREIGN KEY (supply_id) REFERENCES supplies(id);
+
+
 
 CREATE TABLE inventory (
   id int NOT NULL IDENTITY(1,1),
@@ -33,30 +45,94 @@ CREATE TABLE inventory (
   receivables_id int NOT NULL,
   current_count int NOT NULL,
   updated DATETIME NOT NULL
-                DEFAULT CURRENT_TIMESTAMP,
+    DEFAULT CURRENT_TIMESTAMP,
 );
+
+ALTER TABLE inventory ADD PRIMARY KEY (id);
+ALTER TABLE inventory ADD FOREIGN KEY (receivables_id) REFERENCES receivables(id);
 
 CREATE TABLE status (
   id int NOT NULL IDENTITY(1,1),
   name VARCHAR(128) NOT NULL,
 );
 
+ALTER TABLE status ADD PRIMARY KEY (id);
+
+CREATE TABLE serviceSupplies (
+  id int NOT NULL IDENTITY(1,1),
+  supply_id int NOT NULL,
+  qty int NOT NULL,
+);
+
+ALTER TABLE serviceSupplies ADD PRIMARY KEY (id);
+ALTER TABLE serviceSupplies ADD FOREIGN KEY (supply_id) REFERENCES supplies(id);
+
 CREATE TABLE services (
   id int NOT NULL IDENTITY(1,1),
   name VARCHAR(128) NOT NULL,
+  serviceSupplies_id int,
   price float NOT NULL,
   status_id int NOT NULL,
 );
 
+ALTER TABLE services ADD PRIMARY KEY (id);
+ALTER TABLE services ADD FOREIGN KEY (serviceSupplies_id) REFERENCES serviceSupplies(id);
+ALTER TABLE services ADD FOREIGN KEY (status_id) REFERENCES status(id);
+
 CREATE TABLE sales (
   id int NOT NULL IDENTITY(1,1),
   tavern_id int NOT NULL,
-  user_id int NOT NULL,
-  services_id int ,
-  cost int NOT NULL,
+  guests_id int NOT NULL,
+  services_id int,
   created DATETIME NOT NULL
-                DEFAULT CURRENT_TIMESTAMP,
+    DEFAULT CURRENT_TIMESTAMP,
 );
+
+ALTER TABLE sales ADD PRIMARY KEY (id);
+-- ALTER TABLE sales ADD FOREIGN KEY (guests_id) REFERENCES guests(id);
+ALTER TABLE sales ADD FOREIGN KEY (services_id) REFERENCES services(id);
+
+CREATE TABLE class (
+  id int NOT NULL IDENTITY(1,1),
+  name VARCHAR(128) NOT NULL,
+  description VARCHAR(255),
+);
+
+ALTER TABLE class ADD PRIMARY KEY (id);
+
+CREATE TABLE guests (
+  id int NOT NULL IDENTITY(1,1),
+  name VARCHAR(128) NOT NULL,
+  birthday DATETIME NOT NULL,
+  status_id int NOT NULL,
+  tavern_id int NOT NULL,
+  cakeday DATETIME NOT NULL 
+    DEFAULT CURRENT_TIMESTAMP
+);
+
+ALTER TABLE guests ADD PRIMARY KEY (id);
+ALTER TABLE guests ADD FOREIGN KEY (status_id) REFERENCES status(id);
+ALTER TABLE sales ADD FOREIGN KEY (guests_id) REFERENCES guests(id);
+
+CREATE TABLE guestClasses (
+  id int NOT NULL IDENTITY(1,1),
+  guest_id int NOT NULL,
+  lvl int NOT NULL,
+  class_id int NOT NULL 
+)
+
+ALTER TABLE guestClasses ADD PRIMARY KEY (id);
+ALTER TABLE guestClasses ADD FOREIGN KEY (guest_id) REFERENCES guests(id);
+ALTER TABLE guestClasses ADD FOREIGN KEY (class_id) REFERENCES class(id);
+
+CREATE TABLE tavernGuests (
+  id int NOT NULL IDENTITY(1,1),
+  tavern_id int NOT NULL,
+  guest_id int NOT NULL,
+)
+
+ALTER TABLE tavernGuests ADD PRIMARY KEY (id);
+ALTER TABLE tavernGuests ADD FOREIGN KEY (guest_id) REFERENCES guests(id);
 
 CREATE TABLE roles (
   id int NOT NULL IDENTITY(1,1),
@@ -64,33 +140,55 @@ CREATE TABLE roles (
   description VARCHAR(225)
 );
 
+ALTER TABLE roles ADD PRIMARY KEY (id);
+
 CREATE TABLE users (
   id int NOT NULL IDENTITY(1,1),
   name VARCHAR(128) NOT NULL,
-  role_id int NOT NULL,
-  tavern_id int NOT NULL,
+  -- role_id int NOT NULL,
+  -- tavern_id int NOT NULL,
 );
 
-CREATE TABLE basement_rats (
+ALTER TABLE users ADD PRIMARY KEY (id);
+
+CREATE TABLE tavernUsers (
   id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
+  user_id int NOT NULL,
   tavern_id int NOT NULL,
-);
+  role_id int NOT NULL,
+)
+
+ALTER TABLE tavernUsers ADD PRIMARY KEY (id);
+ALTER TABLE tavernUsers ADD FOREIGN KEY (user_id) REFERENCES users(id);
+ALTER TABLE tavernUsers ADD FOREIGN KEY (role_id) REFERENCES roles(id);
 
 CREATE TABLE state (
   id int NOT NULL IDENTITY(1,1),
   name VARCHAR(128) NOT NULL,
 );
 
+ALTER TABLE state ADD PRIMARY KEY (id);
+
 CREATE TABLE tavern (
   id int NOT NULL IDENTITY(1,1),
   name VARCHAR(128) NOT NULL,
-  users_id int ,
+  -- users_id int ,
   address_street VARCHAR(225) NOT NULL,
   address_city VARCHAR(225) NOT NULL,
   state_id int NOT NULL,
   inventory_id int ,
   sales_id int ,
-  floors_count int,
-  basement_rats_id int,
 );
+
+ALTER TABLE tavern ADD PRIMARY KEY (id);
+ALTER TABLE tavern ADD FOREIGN KEY (state_id) REFERENCES state(id);
+ALTER TABLE tavern ADD FOREIGN KEY (inventory_id) REFERENCES inventory(id);
+ALTER TABLE tavern ADD FOREIGN KEY (sales_id) REFERENCES sales(id);
+
+--Adding in FK for tavern_id for above tables
+ALTER TABLE receivables ADD FOREIGN KEY (tavern_id) REFERENCES tavern(id);
+ALTER TABLE inventory ADD FOREIGN KEY (tavern_id) REFERENCES tavern(id);
+ALTER TABLE sales ADD FOREIGN KEY (tavern_id) REFERENCES tavern(id);
+ALTER TABLE guests ADD FOREIGN KEY (tavern_id) REFERENCES tavern(id);
+ALTER TABLE tavernGuests ADD FOREIGN KEY (tavern_id) REFERENCES tavern(id);
+ALTER TABLE tavernUsers ADD FOREIGN KEY (tavern_id) REFERENCES tavern(id);
--- Homework/Assignment02/seedTavernDB.sql
+++ Homework/Assignment02/seedTavernDB.sql
@@ -1,99 +1,4 @@
-USE MASTER;
---drops taverns_db if it exists currently
-DROP DATABASE IF EXISTS taverns_db;
---create taverns_db database
-CREATE DATABASE taverns_db;
-
 USE taverns_db;
-
-CREATE TABLE unit (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-);
-
-CREATE TABLE supplies (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-  unit_id int NOT NULL,
-);
-
-CREATE TABLE receivables (
-  id int NOT NULL IDENTITY(1,1),
-  tavern_id int NOT NULL,
-  supply_id int NOT NULL,
-  qty int NOT NULL,
-  cost float NOT NULL,
-  created DATETIME NOT NULL
-                DEFAULT CURRENT_TIMESTAMP, 
-);
-
-CREATE TABLE inventory (
-  id int NOT NULL IDENTITY(1,1),
-  tavern_id int NOT NULL,
-  receivables_id int NOT NULL,
-  current_count int NOT NULL,
-  updated DATETIME NOT NULL
-                DEFAULT CURRENT_TIMESTAMP,
-);
-
-CREATE TABLE status (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-);
-
-CREATE TABLE services (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-  price float NOT NULL,
-  status_id int NOT NULL,
-);
-
-CREATE TABLE sales (
-  id int NOT NULL IDENTITY(1,1),
-  tavern_id int NOT NULL,
-  user_id int NOT NULL,
-  services_id int ,
-  cost int NOT NULL,
-  created DATETIME NOT NULL
-                DEFAULT CURRENT_TIMESTAMP,
-);
-
-CREATE TABLE roles (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-  description VARCHAR(225)
-);
-
-CREATE TABLE users (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-  role_id int NOT NULL,
-  tavern_id int NOT NULL,
-);
-
-CREATE TABLE basement_rats (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-  tavern_id int NOT NULL,
-);
-
-CREATE TABLE state (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-);
-
-CREATE TABLE tavern (
-  id int NOT NULL IDENTITY(1,1),
-  name VARCHAR(128) NOT NULL,
-  users_id int ,
-  address_street VARCHAR(225) NOT NULL,
-  address_city VARCHAR(225) NOT NULL,
-  state_id int NOT NULL,
-  inventory_id int ,
-  sales_id int ,
-  floors_count int,
-  basement_rats_id int,
-);
 
 INSERT INTO unit VALUES ('handles');
 INSERT INTO unit VALUES ('barrels');
@@ -101,18 +6,36 @@ INSERT INTO unit VALUES ('bottles');
 INSERT INTO unit VALUES ('ounces');
 INSERT INTO unit VALUES ('pounds');
 
+--GO
+
 INSERT INTO supplies VALUES ('ale', 2);
 INSERT INTO supplies VALUES ('whiskey', 2);
 INSERT INTO supplies VALUES ('red wine', 3);
 INSERT INTO supplies VALUES ('white wine', 3);
-INSERT INTO supplies VALUES ('mead', 25);
+INSERT INTO supplies VALUES ('mead', 2);
+
+INSERT INTO state VALUES ('PA');
+INSERT INTO state VALUES ('NJ');
+INSERT INTO state VALUES ('NY');
+INSERT INTO state VALUES ('OH');
+INSERT INTO state VALUES ('DE');
+
+GO
+
+--INSERT INTO tavern VALUES ('Tavern1', '1234 Sesame Street', 'Neshaminy', 1, 1, 4);
+INSERT INTO tavern VALUES ('Tavern1', '1234 Sesame Street', 'Neshaminy', 1, NULL, NULL);
+INSERT INTO tavern VALUES ('Tavern2', '1234 Main Street', 'Allentown', 2, NULL, NULL);
+INSERT INTO tavern VALUES ('Tavern3', '1234 Broad Street', 'Johnstown', 3, NULL, NULL);
+INSERT INTO tavern VALUES ('Tavern4', '1234 Ralph Street', 'Calypso', 4, NULL, NULL);
+INSERT INTO tavern VALUES ('Tavern5', '1234 Jam Street', 'Smithfield', 5, NULL, NULL);
+
+GO
 
 INSERT INTO receivables VALUES (1, 1, 5, 25.00, CURRENT_TIMESTAMP);
 INSERT INTO receivables VALUES (1, 2, 5, 35.00, CURRENT_TIMESTAMP);
 INSERT INTO receivables VALUES (1, 3, 25, 20.00, CURRENT_TIMESTAMP);
 INSERT INTO receivables VALUES (1, 4, 25, 20.00, CURRENT_TIMESTAMP);
 INSERT INTO receivables VALUES (1, 5, 35, 40.00, CURRENT_TIMESTAMP);
-
 
 INSERT INTO inventory VALUES (1, 1, 20, CURRENT_TIMESTAMP);
 INSERT INTO inventory VALUES (1, 1, 25, CURRENT_TIMESTAMP);
@@ -124,18 +47,33 @@ INSERT INTO status VALUES ('active');
 INSERT INTO status VALUES ('inactive');
 INSERT INTO status VALUES ('out of stock');
 INSERT INTO status VALUES ('discontinued');
+INSERT INTO status VALUES ('sick');
+INSERT INTO status VALUES ('fine');
+INSERT INTO status VALUES ('hangry');
+INSERT INTO status VALUES ('raging');
+INSERT INTO status VALUES ('placid');
 
-INSERT INTO services VALUES ('Pool', 1, 1.00);
-INSERT INTO services VALUES ('Weapons Sharpening', 1, 50.00);
-INSERT INTO services VALUES ('Healing', 1, 80.00);
-INSERT INTO services VALUES ('Laundry', 1, 10.00);
-INSERT INTO services VALUES ('Bed', 1, 60.00);
+GO
 
-INSERT INTO sales VALUES (1, 7, 1, 10.00, CURRENT_TIMESTAMP);
-INSERT INTO sales VALUES (1, 6, 2, 150.00, CURRENT_TIMESTAMP);
-INSERT INTO sales VALUES (1, 9, 4, 30.00, CURRENT_TIMESTAMP);
-INSERT INTO sales VALUES (1, 10, 5, 180.00, CURRENT_TIMESTAMP);
-INSERT INTO sales VALUES (1, 8, 3, 80.00, CURRENT_TIMESTAMP);
+INSERT INTO services VALUES ('Pool', NULL, 1.00, 1);
+INSERT INTO services VALUES ('Weapons Sharpening', NULL,  50.00, 1);
+INSERT INTO services VALUES ('Healing', NULL, 80.00, 1);
+INSERT INTO services VALUES ('Laundry', NULL, 10.00, 1);
+INSERT INTO services VALUES ('Bed', NULL, 60.00, 1);
+
+INSERT INTO guests VALUES ('Brock Samson', 01/01/1977,  8, 1, CURRENT_TIMESTAMP);
+INSERT INTO guests VALUES ('The Monarch', 01/01/1951, 7, 1, CURRENT_TIMESTAMP);
+INSERT INTO guests VALUES ('Dr. Girlfriend', 01/01/1979, 6, 1, CURRENT_TIMESTAMP);
+INSERT INTO guests VALUES ('Dean Venture', 01/01/1994, 9, 1, CURRENT_TIMESTAMP);
+INSERT INTO guests VALUES ('Rusty Venture', 01/01/1961, 5, 1, CURRENT_TIMESTAMP);
+
+GO
+
+INSERT INTO sales VALUES (1, 1, 1, CURRENT_TIMESTAMP);
+INSERT INTO sales VALUES (1, 2, 2, CURRENT_TIMESTAMP);
+INSERT INTO sales VALUES (1, 3, 4, CURRENT_TIMESTAMP);
+INSERT INTO sales VALUES (1, 4, 5, CURRENT_TIMESTAMP);
+INSERT INTO sales VALUES (1, 5, 3, CURRENT_TIMESTAMP);
 
 
 INSERT INTO roles VALUES ('Owner', 'owns the tavern');
@@ -145,39 +83,8 @@ INSERT INTO roles VALUES ('Barkeep', 'serves alcholic beverages');
 INSERT INTO roles VALUES ('Janitor', 'cleans of premises');
 INSERT INTO roles VALUES ('Guest', 'guest of th tavern');
 
-INSERT INTO users VALUES ('Captain Michael', 1, 1);
-INSERT INTO users VALUES ('Hal Bryant', 3, 1);
-INSERT INTO users VALUES ('Gail Adams', 4, 1);
-INSERT INTO users VALUES ('Robert Murphy', 2, 1);
-INSERT INTO users VALUES ('Lieutent Sparks', 5, 1);
-INSERT INTO users VALUES ('Scooby', 6, 1);
-INSERT INTO users VALUES ('Velma', 6, 1);
-INSERT INTO users VALUES ('Shaggy', 6, 1);
-INSERT INTO users VALUES ('Daphne', 6, 1);
-INSERT INTO users VALUES ('Fred', 6, 1);
-
-
-INSERT INTO basement_rats VALUES ('Brock Samson', 1);
-INSERT INTO basement_rats VALUES ('The Monarch', 1);
-INSERT INTO basement_rats VALUES ('Dr. Girlfriend', 1);
-INSERT INTO basement_rats VALUES ('Dean Venture', 1);
-INSERT INTO basement_rats VALUES ('Rusty Venture', 1);
-
-INSERT INTO state VALUES ('PA');
-INSERT INTO state VALUES ('NJ');
-INSERT INTO state VALUES ('NY');
-INSERT INTO state VALUES ('OH');
-INSERT INTO state VALUES ('DE');
-
-GO
-
-INSERT INTO tavern VALUES ('Tavern1', NULL, '1234 Sesame Street', 'Neshaminy', 1, 1, 1, 4, 1);
-INSERT INTO tavern VALUES ('Tavern2', NULL, '1234 Main Street', 'Allentown', 2, NULL, NULL, NULL, 2);
-INSERT INTO tavern VALUES ('Tavern3', NULL, '1234 Broad Street', 'Johnsstown', 3, NULL, NULL, NULL, 3);
-INSERT INTO tavern VALUES ('Tavern4', NULL, '1234 Ralph Street', 'Calypso', 4, NULL, NULL, NULL, 4);
-INSERT INTO tavern VALUES ('Tavern5', NULL, '1234 Jam Street', 'Smithfield', 5, NULL, NULL, NULL, 5);
-
-
-
-
-
+INSERT INTO users VALUES ('Captain Michael');
+INSERT INTO users VALUES ('Hal Bryant');
+INSERT INTO users VALUES ('Gail Adams');
+INSERT INTO users VALUES ('Robert Murphy');
+INSERT INTO users VALUES ('Lieutent Sparks');
