ffc764d5cf0aa9d0088ad03cd0dfe3b79110737b
Aja Strommen <a.g.mechanix@gmail.com>
2021-02-03 23:46:53 -0500

    query sort guests asc with classes lvls & labels

--- Homework/Assignment05/Strommen_tavernDB.sql
+++ Homework/Assignment05/Strommen_tavernDB.sql
@@ -14,7 +14,7 @@ Go
 select * from dbo.UserReport()
 	order by RoleName asc;
 
---2 report for guests classes
+--2 query for guests classes
 insert into GuestClass values 
 (1, 1, 3),
 (3, 1, 15),
@@ -32,3 +32,18 @@ select c.Name as Class, count(gc.GuestID) as Guests
 from GuestClass gc
 inner join Class c on gc.ClassID = c.ID
 group by c.Name;
+
+--3 query of guests, classes, & lvls, sorted by name asc
+select g.Name as Guest, c.Name as Class, gc.Level as Level,
+	(case
+	when Level >= 1 and Level < 5
+		then 'begginer'
+	when Level >= 5 and Level < 10
+		then 'intermediate'
+	when Level >=10
+		then 'expert'
+	end) as ClassLabel
+from GuestClass gc
+inner join Guests g on gc.GuestID = g.ID
+inner join Class c on gc.ClassID = c.ID
+order by Guest asc;
