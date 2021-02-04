604e0b8cceb47449641fa762aa5d0434598cf841
Aja Strommen <a.g.mechanix@gmail.com>
2021-02-03 22:58:33 -0500

    query for classes grouped by guests

--- Homework/Assignment05/Strommen_tavernDB.sql
+++ Homework/Assignment05/Strommen_tavernDB.sql
@@ -13,3 +13,22 @@ Go
 
 select * from dbo.UserReport()
 	order by RoleName asc;
+
+--2 report for guests classes
+insert into GuestClass values 
+(1, 1, 3),
+(3, 1, 15),
+(2, 2, 6),
+(3, 3, 50),
+(4, 4, 10),
+(5, 4, 2),
+(1, 5, 25),
+(2, 6, 5),
+(5, 7, 12);
+
+select * from GuestClass;
+
+select c.Name as Class, count(gc.GuestID) as Guests
+from GuestClass gc
+inner join Class c on gc.ClassID = c.ID
+group by c.Name;
